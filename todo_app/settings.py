d485df4e2730bec701c6c06802636912e526db36
Hefajek <rafal.bodanka@gmail.com>
2023-01-04 20:32:03 +0100

    hide secret-key with python-decouple

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$-9vq1_n_mp*mu*e$u$vw2h0e7@j3_(^*_c0b@le$f+o_tv^8d'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
